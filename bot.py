a44afc5da156c9f3464ac306aef69f5de1b988ca
Shogunkayo <pyrotourface@gmail.com>
2022-12-28 15:33:28 +0530

    bot-update-2 added gui buttons

--- bot.py
+++ bot.py
@@ -1,19 +1,68 @@
 import discord
 import responses
+import config
+from discord.ui import Button, View
 
+
+# Create Message View
+
+class SearchView(View):
+    def __init__(self, request, response):
+        super().__init__(timeout=60)
+        self.request = request
+        self.current = 0
+        self.response = response
+
+    @discord.ui.button(label='<<-', custom_id='skip_start')
+    async def skip_start_callback(self, interaction, skip_start_btn):
+        skip_start_btn = [i for i in self.children if i.custom_id == 'skip_start'][0]
+        await interaction.response.send_message('heheheheh')
+
+    @discord.ui.button(label='<-', custom_id='prev')
+    async def prev_callback(self, interaction, prev_btn):
+        prev_btn = [i for i in self.children if i.custom_id == 'prev'][0]
+        prev_btn.label = 'Turipipturip'
+        await interaction.response.send_message('!!!')
+
+    @discord.ui.button(label='->', custom_id='next')
+    async def next_callback(self, interaction, next_btn):
+        next_btn = [i for i in self.children if i.custom_id == 'next'][0]
+        next_btn.label = 'HEHEHEHE'
+        next_btn.disabled = True
+        await interaction.response.edit_message(view=self)
+
+    @discord.ui.button(label='->>', custom_id='skip_end')
+    async def skip_end_callback(self, interaction, skip_end_btn):
+        skip_end_btn = [i for i in self.children if i.custom_id == 'skip_end'][0]
+        await interaction.response.send_message('sdmniasodnasopndsiao')
+
+    async def interaction_check(self, interaction) -> bool:
+        if interaction.user != self.request.author:
+            await interaction.response.send_message('You cant use that', ephemeral=True)
+            return False
+        return True
+
+def create_ui_search(request, response):
+    view = SearchView(request)
+    return view
 
 # Send messages
 async def send_message(message, user_message, is_private):
     try:
-        response = responses.handle_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
+        if(user_message[0] in ['p', 'n', 'c', 'l']):
+            response = responses.handle_response_search(user_message)
+            view = create_ui_search(message, response)
+            await message.author.send(response, view=view) if is_private else await message.channel.send(response, view=view)
+        else:
+            response = responses.handle_response_misc(user_message)
+            await message.author.send(response) if is_private else await message.channel.send(response)
 
     except Exception as e:
         print(e)
 
 
 def run_bot():
-    TOKEN = 'KEY'
+    TOKEN = config.token
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
--- responses.py
+++ responses.py
@@ -1,7 +1,21 @@
+import discord
+
 import random
 import analysis
 
-def handle_response(message) -> str:
+def clean_request(request) -> str:
+    if(request[1] == ' '):
+        request = request[2:]
+        request = request.strip()
+    elif(request[6] == ' '):
+        request = request[7:]
+        request = request.strip()
+    else:
+        request = '0'    
+    
+    return request
+
+def handle_response_misc(message):
     request = message.upper()
 
     if(request == 'HELLO'):
@@ -9,99 +23,76 @@ def handle_response(message) -> str:
     
     if(request == 'ROLL'):
         return str(random.randint(1, 100))
-    
-    if(request[0] == 'P' or request[:6] == 'PLAYER'):
-        try:
-            if(request[1] == ' '):
-                request = request[2:]
-                request = request.strip()
-            elif(request[6] == ' '):
-                request = request[7:]
-                request = request.strip()
-            else:
-                return 'Invalid format'
-        except:
-            return 'Invalid format'
-
-        if(len(request) > 40):
-            return 'Lock off'
-        
-        return '```' + analysis.search_player(request)[:1990] + '```'
-
-    if(request[0] == 'N' or request[:6] == 'NATION'):
-        try:
-            if(request[1] == ' '):
-                request = request[2:]
-                request = request.strip()
-            elif(request[6] == ' '):
-                request = request[7:]
-                request = request.strip()
-            else:
-                return 'Invalid format'
-        except:
-            return 'Invalid format'
-
-        if(len(request) > 40):
-            return 'Lock off'
-        
-        return '```' + analysis.search_nation(request)[:1990] + '```'
-
-    
-    if(request[0] == 'C' or request[:6] == 'CLUB'):
-        try:
-            if(request[1] == ' '):
-                request = request[2:]
-                request = request.strip()
-            elif(request[6] == ' '):
-                request = request[7:]
-                request = request.strip()
-            else:
-                return 'Invalid format'
-        except:
-            return 'Invalid format'
-
-        if(len(request) > 40):
-            return 'Lock off'
-        
-        return '```' + analysis.search_club(request)[:1990] + '```'
-    
-    if(request[0] == 'L' or request[:6] == 'LEAGUE'):
-        try:
-            if(request[1] == ' '):
-                request = request[2:]
-                request = request.strip()
-            elif(request[6] == ' '):
-                request = request[7:]
-                request = request.strip()
-            else:
-                return 'Invalid format'
-        except:
-            return 'Invalid format'
-
-        if(len(request) > 40):
-            return 'Lock off'
-        
-        return '```' + analysis.search_league(request)[:1990] + '```'
-
-    if(request[0] == 'POS' or request[:6] == 'POSITION'):
-        try:
-            if(request[1] == ' '):
-                request = request[2:]
-                request = request.strip()
-            elif(request[6] == ' '):
-                request = request[7:]
-                request = request.strip()
-            else:
-                return 'Invalid format'
-        except:
-            return 'Invalid format'
-
-        if(len(request) > 40):
-            return 'Lock off'
-        
-        return '```' + analysis.search_position(request)[:1990] + '```'
 
     if(request == 'help'):
         return '>>> Teehee'
     
     return 'I have no response for this'
+
+
+def handle_response_search(message):
+    request = message.upper()
+
+    if(request[0] == 'P' or request[:6] == 'PLAYER'):
+        request = clean_request(request)
+        if(request == '0' or len(request) > 40):
+            return 'Lock off'
+        
+        return '```' + str(analysis.search_player(request)) + '```'
+
+
+    if(request[0] == 'N' or request[:6] == 'NATION'):
+        request = clean_request(request)
+        if(request == '0' or len(request) > 40):
+            return 'Lock off'
+    
+        return '```' + analysis.search_nation(request) + '```'
+
+    
+    if(request[0] == 'C' or request[:6] == 'CLUB'):
+        request = clean_request(request)
+        if(request == '0' or len(request) > 40):
+            return 'Lock off'
+
+        return '```' + analysis.search_club(request)[:1990] + '```'
+    
+
+    if(request[0] == 'L' or request[:6] == 'LEAGUE'):
+        request = clean_request(request)
+        if(request == '0' or len(request) > 40):
+            return 'Lock off'
+        
+        return '```' + analysis.search_league(request)[:1990] + '```'
+
+
+    if(request[0] == 'POS' or request[:6] == 'POSITION'):
+        request = clean_request(request)
+        if(request == '0' or len(request) > 40):
+            return 'Lock off'
+        
+        return '```' + analysis.search_position(request)[:1990] + '```'
+
+'''
+>>>
+---------------------------------------------
+| + Name: Lionel Messi  + Position: CF      |
+| + Club: Paris SG      + Ovr Rating: 94    |
+| + Nation: Argentina   + Base Rating: 477  |
+| + League: Ligue 1     + Price: 1935000    |
+| ------------------------------------------|
+| + Name: Lionel Messi  + Position: CF      |
+| + Club: Paris SG      + Ovr Rating: 94    |
+| + Nation: Argentina   + Base Rating: 477  |
+| + League: Ligue 1     + Price: 1935000    |
+| ------------------------------------------|
+| + Name: Lionel Messi  + Position: CF      |
+| + Club: Paris SG      + Ovr Rating: 94    |
+| + Nation: Argentina   + Base Rating: 477  |
+| + League: Ligue 1     + Price: 1935000    |
+| ------------------------------------------|
+| + Name: Lionel Messi  + Position: CF      |
+| + Club: Paris SG      + Ovr Rating: 94    |
+| + Nation: Argentina   + Base Rating: 477  |
+| + League: Ligue 1     + Price: 1935000    |
+---------------------------------------------
+'''
